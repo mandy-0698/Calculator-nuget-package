55186ccf4a0f6bea002faa1c946f0eb9f9fd19af
Mritunjay Yadav <mritunjay.yadav@mrisoftware.com>
2024-03-22 14:43:16 +0530

    Pushed code

---
+++ Calculator-nuget-demo/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using Calculator_nuget_demo.Contracts;
+
+namespace Calculator_nuget_demo
+{
+    public class Calculator : ICalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Calculator-nuget-demo/Contracts/ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_nuget_demo.Contracts
+{
+    public interface ICalculator
+    {
+        public int Add(int a, int b);
+        public int Subtract(int a, int b);
+        public int Multiply(int a, int b);
+        public int Divide(int a, int b);
+    }
+}
